bbf6a7d4ebffbff7bbe1c2336eefe0dce01b4f46
alkarawim <159176701+alkarawim@users.noreply.github.com>
2024-04-26 09:55:02 +0200

    done
    done

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -5,11 +5,9 @@
 
 let apMap = L.map('apMap').setView([51.23009, 4.41616], 17);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoibGVjdG9yd291dGVyIiwiYSI6ImNrM25qZWs1dTB4NHgza240bW0zOG1qZngifQ.1uF5JjJA8l5SpTW3NVQJJQ'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(apMap);
 
 // bepaal de rechthoek rondom het gebouw van AP
